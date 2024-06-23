36a0d0a65e81f75cb1b1a580b830ec3680eafe62
gizziro <137782373+gizziro@users.noreply.github.com>
2024-06-23 13:19:47 +0900

    add event

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,27 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <input v-model="initText"></input>
+    <button @click="clickEvent">click</button>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      initText : "hello",
+    }
+  },
+  methods : {
+    clickEvent() {
+      axios.get('/api/test')
+     .then(res => {this.initText = res.text})
+    }
+  }
+}
+</script>
 
 <style>
 @media (min-width: 1024px) {
